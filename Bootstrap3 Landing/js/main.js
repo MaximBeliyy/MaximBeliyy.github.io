7a972dbee432194a8888377b26415d7be3bc5188
Maxim <maximmbeliy90.@gmail.com>
2019-03-20 16:13:24 +0200

    commit

---
+++ Bootstrap3 Landing/js/main.js
@@ -0,0 +1,12 @@
+(function($) {
+	"use strict"
+	
+	$(window).on('load', function() {
+		$("#preloader").delay(600).fadeOut();
+	});
+
+	$('.navbar-toggle').on('click',function(){
+		$('#header').toggleClass('nav-collapse')
+	});
+	
+})(jQuery);
